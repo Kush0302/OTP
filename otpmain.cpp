bc243d5b747593fc5d68ed5021d0800ad22de910
Kush0302 <144573739+Kush0302@users.noreply.github.com>
2023-09-10 08:25:24 +0530

    Add files via upload

---
+++ otpmain.cpp
@@ -0,0 +1,89 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+using namespace std;
+void randomPasswordGeneration(int N)
+{
+int i = 0;
+int randomizer = 0;
+srand((unsigned int)(time(NULL)));
+    char numbers[] = "0123456789";
+
+    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+
+    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+    char symbols[] = "!@#$^&*?";
+
+
+    char password[N];
+
+
+    randomizer = rand() % 4;
+
+
+    for (i = 0; i < N; i++)
+        {
+
+        if (randomizer == 1)
+            {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2)
+        {
+            password[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3)
+        {
+            password[i] = LETTER[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else
+        {
+            password[i] = letter[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+}
+int main()
+{
+string username;
+string password;
+read:
+    cout<<"-----------------------------";
+    cout<<"\n       WELCOME USER           ";
+    cout<<"\n-----------------------------"<<endl;
+    cout<<" Enter your username: " << endl;
+    cin >> username;
+    cout << "Enter your password: " << endl;
+    cin >> password;
+{
+    if( username!="polygon" && password!="mecia")
+    {
+        cout << "Incorrect username/password combination. Please try again." << "\n" <<endl;
+        goto read;
+
+    }
+    else
+    {
+    cout<<"Access granted."<<endl;
+
+    }
+}
+
+    int N = 10;
+    cout<<"\n your OTP is:";
+
+    randomPasswordGeneration(N);
+    cout<<"\n USE THIS OTP TO ACCESS YOUR E.V.";
+
+    return 0;
+}
